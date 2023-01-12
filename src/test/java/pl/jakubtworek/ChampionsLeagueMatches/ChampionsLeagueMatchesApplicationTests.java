0e0753892a2ff96c7810b5c6c5438c3d0abe913c
KubaTworek <kubatvorek@gmail.com>
2023-01-12 20:57:10 +0100

    init commit

---
+++ src/test/java/pl/jakubtworek/ChampionsLeagueMatches/ChampionsLeagueMatchesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jakubtworek.ChampionsLeagueMatches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChampionsLeagueMatchesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
