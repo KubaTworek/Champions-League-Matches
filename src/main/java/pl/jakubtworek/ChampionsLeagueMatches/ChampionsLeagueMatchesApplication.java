c3bae0ca6e7f3becc15bb99d50fbc9738c335101
KubaTworek <kubatvorek@gmail.com>
2023-01-16 19:44:04 +0100

    clean up

--- src/main/java/pl/jakubtworek/ChampionsLeagueMatches/ChampionsLeagueMatchesApplication.java
+++ src/main/java/pl/jakubtworek/ChampionsLeagueMatches/ChampionsLeagueMatchesApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChampionsLeagueMatchesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChampionsLeagueMatchesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ChampionsLeagueMatchesApplication.class, args);
+    }
 
 }
